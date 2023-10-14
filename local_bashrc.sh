345831d7e391fa09dec494cc04eab407dbd94641
Carlo Emilio MONTANARI <carlo.emilio.montanari@cern.ch>
2023-10-14 16:11:04 +0200

    Had some problems with two sbp lines

--- local_bashrc.sh
+++ local_bashrc.sh
@@ -4,8 +4,11 @@ export EDITOR=code
 
 SBP_PATH=/home/camontan/my_bashtools/sbp
 source /home/camontan/my_bashtools/sbp/sbp.bash
-sbp set layout powerline
-sbp set color default
+
+# Had some problems with these two lines, so I hacked them out and altered
+# the sbp files to not use them
+# sbp set layout powerline
+# sbp set color default
 
 # Set home directory alias
 alias home='cd ~'
